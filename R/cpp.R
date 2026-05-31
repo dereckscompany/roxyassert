e7a265d029eaf7013d7831789b3706378cf1e603
Dereck Mezquita <dereck@mezquita.io>
2026-05-30 18:55:25 -0600

    Strip Rcpp scaffold; depend on assert + roxygen2
    Remove the template's Rcpp example (src/, RcppExports, add()/sum_array_cpp and
    their tests, .clang-format). Add Imports: assert, roxygen2 (+ Remotes for
    assert). Rewrite the Description to credit the assert package. Drop the deleted
    functions from _pkgdown.yml and rewrite the vignette as a short overview that
    builds without them. Add a package-doc stub.

--- R/cpp.R
+++
@@ -1,6 +0,0 @@
-## usethis namespace: start
-#' @useDynLib roxyassert, .registration=TRUE
-#' @importFrom Rcpp sourceCpp
-#' @importFrom Rcpp evalCpp
-## usethis namespace: end
-NULL
--- src/sum_array.h
+++
@@ -1,6 +0,0 @@
-#ifndef TEMPLATE_R_PACKAGE_SUM_ARRAY_CPP
-#define TEMPLATE_R_PACKAGE_SUM_ARRAY_CPP
-
-double sum_array_cpp(NumericVector x);
-
-#endif
